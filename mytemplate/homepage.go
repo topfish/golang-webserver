01b98d1d3a270645bead6c963924e6a66cc3d57c
yuyang <topyyu@126.com>
2021-08-19 14:35:18 +0800

    update

--- mytemplate/homepage.go
+++ mytemplate/homepage.go
@@ -4,6 +4,8 @@ import(
 	"net/http"
 	"html/template"
 	"time"
+	"io/ioutil"
+	"fmt"
 )
 
 func MyProcess(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,17 @@ func MyProcess(w http.ResponseWriter, r *http.Request) {
 	DeleteTemp(filename)
 }
 
+func checkPagefile() {
+	_, err := ioutil.ReadFile("/etc/webapp/index.html")
+	if err != nil {
+		fmt.Println("==== please config index.html ====")
+		fmt.Println("==== you can execute like this: ====")
+		fmt.Println("==== mkdir -p /etc/webapp &&  cp ./mytemplate/index.html /etc/webapp/index.html ====")
+	}
+}
+
 func MyHome(w http.ResponseWriter, r *http.Request) {
+	checkPagefile()
 	t, _ := template.ParseFiles("/etc/webapp/index.html")
 	t.Execute(w, time.Now())
 }
