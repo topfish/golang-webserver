afbcb8542b1a32469c6742f47fe405c89f48b717
fish <topyyu@126.com>
2024-06-21 10:45:37 +0800

    删除缓存文件index.html

--- go-web/webserver.go
+++ go-web/webserver.go
@@ -1,18 +1,36 @@
 package main
 
-import(
+import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 	"webapp/myserver"
 	"webapp/mytemplate"
-	"fmt"
 )
 
 func main() {
+	defer cleanUp()
+
 	server := http.Server{
 		Addr: ":80",
 	}
 	fmt.Println("Hello I Am WebServer")
 
+	go func() {
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGINT)
+		<-sig
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			panic(err)
+		}
+	}()
 	http.HandleFunc("/", mytemplate.MyHome)
 	http.HandleFunc("/list", myserver.ServerList)
 	http.HandleFunc("/getcookie", myserver.Mysetcookie)
@@ -28,5 +46,17 @@ func main() {
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
+	}
+
+}
+
+func cleanUp() {
+	//index.html缓存清理
+	err := os.Remove("index.html")
+	//_, err := os.ReadFile("index.html")
+	if err != nil {
+		log.Println("WARNING: delete cache file index.html failed")
+	} else {
+		log.Println("clearn cache finshed")
 	}
 }
